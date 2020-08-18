6111aeb61e464c242d7792e33909dce78159d3c1
jstyle5 <54833010+jstyle5@users.noreply.github.com>
2020-08-18 21:07:10 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,30 +4,26 @@ import InputRange from 'react-input-range';
 import './styles.css';
 
 class TimeRangeSlider extends Component {
-
   constructor(props) {
     super(props);
-    this.state = {
-    }
+    this.state = {};
   }
 
-  componentDidMount(){
-  }
+  componentDidMount() {}
 
-  componentWillUnmount(){
-  }
+  componentWillUnmount() {}
 
-  minuteToTime(value){
-    value = (value > 1439) ? 1439 : value;
+  minuteToTime(value) {
+    value = value > 1439 ? 1439 : value;
     let hours = Math.floor(value / 60),
-    minutes = value - (hours * 60),
-    ampm = null;
+      minutes = value - hours * 60,
+      ampm = null;
 
     if (hours.length == 1) hours = '0' + hours;
     if (minutes.length == 1) minutes = '0' + minutes;
     if (minutes == 0) minutes = '00';
-    if(this.props.format == 12){
-      ampm = "AM";
+    if (this.props.format == 12) {
+      ampm = 'AM';
       if (hours >= 12) {
         if (hours == 12) {
           hours = hours;
@@ -36,7 +32,7 @@ class TimeRangeSlider extends Component {
           hours = hours - 12;
           minutes = minutes;
         }
-        ampm = "PM";
+        ampm = 'PM';
       }
       if (hours == 0) {
         hours = 12;
@@ -44,81 +40,78 @@ class TimeRangeSlider extends Component {
       }
     }
 
-    return {hours: hours, minutes: minutes, am_pm: ampm};
+    return { hours: hours, minutes: minutes, am_pm: ampm };
   }
 
-  timeToMinute(time){
+  timeToMinute(time) {
     let rMinutes = 1439;
-    if(this.props.format == 24){
-      time = time.split(":");
-      if(time.length < 2){
-        throw new Error("Invalid time");
+    if (this.props.format == 24) {
+      time = time.split(':');
+      if (time.length < 2) {
+        throw new Error('Invalid time');
       }
       let hours = time[0],
-      minutes = parseInt(time[1]);
+        minutes = parseInt(time[1]);
       hours = parseInt(hours * 60);
       rMinutes = hours + minutes;
-    }else{
+    } else {
       time = time.toUpperCase();
-      time = time.replace(" ", "");
-      let ampm = (time.indexOf("AM") != -1) ? "AM" : "PM";
-      time = time.replace(ampm, "");
-      time = time.split(":");
-      if(time.length < 2){
-        throw new Error("Invalid time");
+      time = time.replace(' ', '');
+      let ampm = time.indexOf('AM') != -1 ? 'AM' : 'PM';
+      time = time.replace(ampm, '');
+      time = time.split(':');
+      if (time.length < 2) {
+        throw new Error('Invalid time');
       }
       let hours = parseInt(time[0]),
-      minutes = parseInt(time[1]);
-      if(ampm == "PM"){
-        if(hours != 12){
-          hours = (hours + 12);
+        minutes = parseInt(time[1]);
+      if (ampm == 'PM') {
+        if (hours != 12) {
+          hours = hours + 12;
         }
-      }else{
-        hours = (hours == 12) ? 0 : hours;
+      } else {
+        hours = hours == 12 ? 0 : hours;
       }
       hours = hours * 60;
       rMinutes = hours + minutes;
     }
-    return (rMinutes > 1439) ? 1439 : rMinutes;
+    return rMinutes > 1439 ? 1439 : rMinutes;
   }
 
-  onChange(value){
+  onChange(key, value) {
     let start = this.minuteToTime(value.min);
     let end = this.minuteToTime(value.max);
-    let nStart = start.hours + ":" + start.minutes;
-    let nEnd = end.hours + ":" + end.minutes;
-    if(this.props.format == 12){
-      nStart += " " + start.am_pm;
-      nEnd += " " + end.am_pm;
+    let nStart = start.hours + ':' + start.minutes;
+    let nEnd = end.hours + ':' + end.minutes;
+    if (this.props.format == 12) {
+      nStart += ' ' + start.am_pm;
+      nEnd += ' ' + end.am_pm;
     }
     this.props.onChange({
-      start: nStart,
-      end: nEnd
+      [key]: { start: nStart, end: nEnd },
     });
   }
 
-  onChangeComplete(value){
+  onChangeComplete(key, value) {
     let start = this.minuteToTime(value.min),
-    end = this.minuteToTime(value.max);
+      end = this.minuteToTime(value.max);
     this.props.onChangeComplete({
-      start: start,
-      end: end
+      [key]: { start: start, end: end },
     });
   }
 
-  onChangeStart(value){
+  onChangeStart(key, value) {
     let start = this.minuteToTime(value.min),
-    end = this.minuteToTime(value.max);
+      end = this.minuteToTime(value.max);
     this.props.onChangeStart({
-      start: start,
-      end: end
+      [key]: { start: start, end: end },
     });
   }
 
   render() {
-    let {start, end} = this.props.value,
-    min = this.timeToMinute(start),
-    max = this.timeToMinute(end);
+    let { key } = this.props.value;
+    let { start, end } = key;
+    (min = this.timeToMinute(start)), (max = this.timeToMinute(end));
     return (
       <InputRange
         disabled={this.props.disabled}
@@ -129,7 +122,9 @@ class TimeRangeSlider extends Component {
         onChange={this.onChange.bind(this)}
         onChangeComplete={this.onChangeComplete.bind(this)}
         step={this.props.step}
-        value={{min: min, max: max}}/>);
+        value={{ min: min, max: max }}
+      />
+    );
   }
 }
 
@@ -137,16 +132,16 @@ TimeRangeSlider.defaultProps = {
   disabled: false,
   draggableTrack: false,
   format: 24,
-  maxValue: "23:59",
-  minValue: "00:00",
+  maxValue: '23:59',
+  minValue: '00:00',
   onChange: () => {},
   onChangeComplete: () => {},
   onChangeStart: () => {},
   step: 15,
   value: {
-    start: "00:00", 
-    end: "23:59"
-  }
-}
+    start: '00:00',
+    end: '23:59',
+  },
+};
 
 export default TimeRangeSlider;
